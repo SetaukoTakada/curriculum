24499fb2ce40703f857e5e2aaaf8a233cbfb5352
高田セツ子 <122262042+SetaukoTakada@users.noreply.github.com>
2023-02-03 21:47:30 +0900

    Add files via upload

--- 3-1/index.php
+++ 3-1/index.php
@@ -1,15 +1,14 @@
 <?php
-for ($i=1; $i <=100; $i++) { 
+for ($i = 1;$i <= 100;$i++){
   echo '<br>';
-    if ($i % 3 == 0) {
+   if ($i % 3 == 0 && $i % 5 == 0) {
+     echo "FizzBuzz!!";
+   }elseif ($i % 3 == 0) {
     echo "Fizz!";
-  }elseif ($i % 5 == 0) {
+   }elseif ($i % 5 == 0) {
     echo "Buzz!";
-  }elseif ($i % 3 == 0 && $i % 5 == 0) {
-    echo "FizzBuzz!!";
-  }else {
-    echo $i;
-  }
+   }else
+   echo $i;
 }
-
+ 
 ?>
